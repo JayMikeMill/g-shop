0363deeef94d49a6495f18b1c8bef0841b3c7cd8
Jason Millis <jaymikemill@gmail.com>
2025-10-02 17:29:44 -0600

    chore: add shared folder to global TS inclue

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 
-import type { Product, ProductVariant } from "@shared/types/Product";
+import type { ProductVariant } from "@shared/types/Product";
 
 // Cart state management
 import { useCart } from "@features/cart/useCart";
--- shared/types/crud-interface.ts
+++
@@ -1,9 +0,0 @@
-import type { QueryObject } from "./QueryObject";
-
-export interface CRUDInterface<T> {
-  create(data: Partial<T>): Promise<T>;
-  get(id: string): Promise<T | null>;
-  getAll(query?: QueryObject): Promise<{ data: T[]; total: number }>;
-  update(updates: Partial<T> & { id: string }): Promise<T>;
-  delete(id: string): Promise<T>;
-}
