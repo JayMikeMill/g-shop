e14e2f1585c44915026798ee9a68d31a18cc950b
Jason Millis <jaymikemill@gmail.com>
2025-10-17 14:10:22 -0600

    fix: Firebase issue and backend db tracking

--- backend/src/adapters/firebase/FirebaseStorageAdapter.ts
+++ backend/src/adapters/firebase/FirebaseStorageAdapter.ts
@@ -1,33 +1,9 @@
 import { StorageAdapter } from "@adapters/types";
 import { getStorage } from "firebase-admin/storage";
-import { initializeApp, cert, App } from "firebase-admin/app";
-import path from "path";
-import fs from "fs";
 
 // Initialize Firebase Admin if not already initialized
 
 // Only initialize if not already initialized
-let app: App | undefined;
-try {
-  app = initializeApp({
-    credential: cert(
-      JSON.parse(
-        fs.readFileSync(
-          path.resolve(
-            __dirname,
-            "config/NailStoreFirebaseServiceAccount.json"
-          ),
-          "utf8"
-        )
-      )
-    ),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  });
-} catch (e: any) {
-  // If already initialized, ignore error
-  if (!/already exists/u.test(e.message)) throw e;
-}
-
 export class FirebaseStorageAdapter implements StorageAdapter {
   private storage = getStorage();
   private bucket = this.storage.bucket();
