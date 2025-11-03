1328505f911f9b6f5c3ebbb17771b92aad875a3b
Jason Millis <jaymikemill@gmail.com>
2025-11-03 11:43:15 -0600

    feat: make backend cross env compatible

--- backend/prisma/backup.js
+++ backend/prisma/backup.js
@@ -1,6 +1,23 @@
 #!/usr/bin/env node
 const { execSync } = require("child_process");
 
+// pg_dump --dbname="postgresql://postgres.xbbovndnldxrkoozastv:GShopY3`$hua711@aws-1-us-east-2.pooler.supabase.com:5432/postgres" --file="gshop_backup.sql"
+// psql "postgresql://postgres:GShopY3`$hua711@gshop-db.czowauoie5ln.us-east-2.rds.amazonaws.com:5432/gshop" -f "gshop_backup.sql"
+// pg_dump --format=custom --no-owner --no-acl --dbname="postgresql://postgres.xbbovndnldxrkoozastv:GShopY3`$hua711@aws-1-us-east-2.pooler.supabase.com:5432/postgres" --file="supabase_clean.dump"
+
+// pg_dump --format=custom --no-owner --no-acl `
+//   --exclude-extension=pg_graphql `
+//   --exclude-extension=supabase_vault `
+//   --exclude-schema=auth `
+//   --exclude-schema=graphql `
+//   --exclude-schema=vault `
+//   --exclude-schema=storage `
+//   --exclude-schema=pgbouncer `
+//   --dbname="postgresql://postgres.xbbovndnldxrkoozastv:GShopY3`$hua711@aws-1-us-east-2.pooler.supabase.com:5432/postgres" `
+//   --file="gshop.dump"
+
+// pg_restore --dbname="postgresql://postgres.ryqzaybytwfqsppjhmvw:Chuck`$h0p2025@aws-1-us-east-2.pooler.supabase.com:5432/postgres" --clean --if-exists --no-owner --no-acl "gshop.dump"
+
 // Function to generate readable timestamp without seconds
 function getTimestamp() {
   const pad = (n) => (n < 10 ? "0" + n : n);
